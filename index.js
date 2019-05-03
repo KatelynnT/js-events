a8c10aa89035290ead2861fb977b5b7d44f2e5ba
Rob Hess <robwhess@gmail.com>
2019-05-03 09:55:49 -0700

    Add code from 5/3 lecture.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-window.addEventListener('click', function (event) {
-  console.log("== Window was clicked, event:");
-});
+// window.addEventListener('click', function (event) {
+//   console.log("== Window was clicked, event:");
+// });
 
 function boxClickListener(event) {
   console.log("== A box was clicked");
@@ -8,20 +8,29 @@ function boxClickListener(event) {
   console.log("  -- event.currentTarget:", event.currentTarget);
 }
 
-var boxes = document.getElementsByClassName('box');
-for (var i = 0; i < boxes.length; i++) {
-  boxes[i].addEventListener('click', boxClickListener);
-}
+// var boxes = document.getElementsByClassName('box');
+// for (var i = 0; i < boxes.length; i++) {
+//   boxes[i].addEventListener('click', boxClickListener);
+// }
 
-var buttons = document.getElementsByClassName('in-box-button');
-for (var i = 0; i < boxes.length; i++) {
-  buttons[i].addEventListener('click', function (event) {
-    console.log("== A button was clicked");
-    event.stopPropagation();
-    var boxParent = event.currentTarget.parentNode;
-    boxParent.classList.toggle('highlighted');
-  });
-}
+// var buttons = document.getElementsByClassName('in-box-button');
+// for (var i = 0; i < buttons.length; i++) {
+//   buttons[i].addEventListener('click', function (event) {
+//     console.log("== A button was clicked");
+//     event.stopPropagation();
+//     var boxParent = event.currentTarget.parentNode;
+//     boxParent.classList.toggle('highlighted');
+//   });
+// }
+
+var boxContainer = document.getElementById('box-container');
+boxContainer.addEventListener('click', function (event) {
+  console.log("== Container clicked, event.target:", event.target);
+  if (event.target.classList.contains('in-box-button')) {
+    console.log("  -- A button was clicked");
+    event.target.parentNode.classList.toggle('highlighted');
+  }
+});
 
 var websiteLink = document.getElementById('website-link');
 websiteLink.addEventListener('click', function (event) {
--- wordBoxes.js
+++ wordBoxes.js
@@ -2,8 +2,13 @@ var allWords = [];
 var currentWord = 0;
 
 function handleNewWordsEntered(event) {
-  var text = '';
+  console.log("== textContent", event.currentTarget.textContent);
+  var text = event.currentTarget.value;
   allWords = text.replace(/[!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~]/g, '')
     .toLowerCase().split(' ');
   currentWord = 0;
+  console.log("== New words:", allWords);
 }
+
+var wordsInput = document.getElementById('words-input');
+wordsInput.addEventListener('change', handleNewWordsEntered);
